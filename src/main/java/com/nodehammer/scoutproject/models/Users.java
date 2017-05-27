1159986a4b17ff5638cd273546230416ac5d250d
Darren Sherrill <darrencsherrill@gmail.com>
2017-05-27 01:47:45 -0500

    users

--- src/main/java/com/nodehammer/scoutproject/models/Users.java
+++ src/main/java/com/nodehammer/scoutproject/models/Users.java
@@ -4,4 +4,31 @@ package com.nodehammer.scoutproject.models;
  * Created by Darren on 5/19/2017.
  */
 public class Users {
+
+    private String username;
+    private String password;
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public Users(String username, String password) {
+        this.username = username;
+        this.password = password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+
+
 }
